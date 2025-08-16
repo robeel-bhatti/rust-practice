5ede307d8d8314e8c27e2e6b37059afbd7381060
Robeel Bhatti <robeelbhatti19@gmail.com>
2025-08-15 23:41:19 -0400

    first commit.

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let even: Vec<i32> = vec![1,2,3,4,5,6,7,8,9,10];
+    let odd: Vec<i32> = vec![1,2,3,4,5, 6, 7, 8, 9];
+}
+
+fn get_median(mut values: &Vec<i32>) -> i32 {
+
+}
+
+fn get_mode() {
+
+}
