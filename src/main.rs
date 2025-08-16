5365efa3847112153aa0728a4d3f2b15f3396f47
Robeel Bhatti <robeelbhatti19@gmail.com>
2025-08-16 00:30:52 -0400

    feat: median and mode vector practice.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,44 @@
+use std::collections::HashMap;
+
 fn main() {
-    let even: Vec<i32> = vec![1,2,3,4,5,6,7,8,9,10];
-    let odd: Vec<i32> = vec![1,2,3,4,5, 6, 7, 8, 9];
+    let mut even: Vec<i32> = vec![1,2,3,4,5,6,7,8,9,10];
+    let mut odd: Vec<i32> = vec![1,2,3,4,5, 6, 7, 8, 9];
+
+    assert_eq!(get_median(&mut even), 5.5);
+    assert_eq!(get_median(&mut odd), 5.0);
+
+    assert_eq!(get_mode(&even), -1);
+    assert_eq!(get_mode(&odd), -1);
 }
 
-fn get_median(mut values: &Vec<i32>) -> i32 {
+fn get_median(values: &mut Vec<i32>) -> f64 {
+    values.sort();
+    let vl = values.len();
+    let mid = vl / 2;
 
+    if vl % 2 == 0 {
+        (values[mid-1] + values[mid]) as f64 / 2.0
+    } else {
+        values[mid] as f64
+    }
 }
 
-fn get_mode() {
+fn get_mode(values: &Vec<i32>) -> i32 {
+    let mut count: HashMap<i32, i32> = HashMap::new();
 
+    for &i in values {
+        *count.entry(i).or_insert(0) += 1;
+    }
+
+    let mut highest_key = -1;
+    let mut highest_value = -1;
+
+    for (k, v) in &count {
+        if *v > highest_value {
+            highest_value = *v;
+            highest_key = *k;
+        }
+    }
+
+    highest_key
 }
