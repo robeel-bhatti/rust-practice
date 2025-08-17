c85797df5e98e27985e78f1ff882bbce6afaf7aa
Robeel Bhatti <robeelbhatti19@gmail.com>
2025-08-17 00:17:06 -0400

    feat: update logic.

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,11 @@ fn main() {
     let mut even: Vec<i32> = vec![1,2,3,4,5,6,7,8,9,10];
     let mut odd: Vec<i32> = vec![1,2,3,4,5, 6, 7, 8, 9];
 
-    assert_eq!(get_median(&mut even), 5.5);
-    assert_eq!(get_median(&mut odd), 5.0);
+    println!("Median of list 1 is {}", get_median(&mut even));
+    println!("Mode of list 1 is {}", get_mode(&even));
 
-    assert_eq!(get_mode(&even), -1);
-    assert_eq!(get_mode(&odd), -1);
+    println!("Median of list 2 is {}", get_median(&mut odd));
+    println!("Mode of list 2  is {}", get_mode(&odd));
 }
 
 fn get_median(values: &mut Vec<i32>) -> f64 {
@@ -24,21 +24,19 @@ fn get_median(values: &mut Vec<i32>) -> f64 {
 }
 
 fn get_mode(values: &Vec<i32>) -> i32 {
-    let mut count: HashMap<i32, i32> = HashMap::new();
+    let mut freq_map: HashMap<i32, i32> = HashMap::new();
 
     for &i in values {
-        *count.entry(i).or_insert(0) += 1;
+        *freq_map.entry(i).or_insert(0) += 1;
     }
 
-    let mut highest_key = -1;
-    let mut highest_value = -1;
+    let max_freq: &i32 = freq_map.values().max().unwrap_or(&0);
 
-    for (k, v) in &count {
-        if *v > highest_value {
-            highest_value = *v;
-            highest_key = *k;
+    for (k, v) in &freq_map {
+        if *v == *max_freq {
+            return *k;
         }
     }
 
-    highest_key
+    *max_freq
 }
