022045e22905225490484cc5b9ed52b634fc97b8
Robeel Bhatti <robeelbhatti19@gmail.com>
2025-08-17 00:18:10 -0400

    feat: update logic.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use std::collections::HashMap;
+mod median_and_mode;
+
+use median_and_mode::get_median;
+use median_and_mode::get_mode;
 
 fn main() {
     let mut even: Vec<i32> = vec![1,2,3,4,5,6,7,8,9,10];
@@ -11,32 +14,4 @@ fn main() {
     println!("Mode of list 2  is {}", get_mode(&odd));
 }
 
-fn get_median(values: &mut Vec<i32>) -> f64 {
-    values.sort();
-    let vl = values.len();
-    let mid = vl / 2;
 
-    if vl % 2 == 0 {
-        (values[mid-1] + values[mid]) as f64 / 2.0
-    } else {
-        values[mid] as f64
-    }
-}
-
-fn get_mode(values: &Vec<i32>) -> i32 {
-    let mut freq_map: HashMap<i32, i32> = HashMap::new();
-
-    for &i in values {
-        *freq_map.entry(i).or_insert(0) += 1;
-    }
-
-    let max_freq: &i32 = freq_map.values().max().unwrap_or(&0);
-
-    for (k, v) in &freq_map {
-        if *v == *max_freq {
-            return *k;
-        }
-    }
-
-    *max_freq
-}
